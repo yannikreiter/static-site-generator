4bf79d76afc60b994c9d095e48f13315ab7899b9
Yannik Reiter <yannik.reiter@maxworx.com>
2025-05-07 09:37:38 +0200

    Implement HTMLNode class and unit tests

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+	def __init__(self, tag = None, value = None, children = None, props = None):
+		self.tag = tag
+		self.value = value
+		self.children = children
+		self.props = props
+	
+	def to_html(self):
+		raise NotImplementedError()
+	
+	def props_to_html(self):
+		html = ""
+		for key in self.props:
+			html += f' {key}="{self.props[key]}"'
+		return html
+
+	def __repr__(self):
+		return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+from htmlnode import HTMLNode
 from textnode import TextNode, TextType
+
 
 def main():
 	text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+	html_node = HTMLNode("p", "This is a paragraph", None, { "class": "bold" })
 	print(text_node)
+	print(html_node)
 
 main()
